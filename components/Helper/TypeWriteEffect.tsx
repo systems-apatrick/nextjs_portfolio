ec1fd1eaeef73c7c4f09d77d1767ad7f48c72d05
Patricio Fajardo <ba010006d6@baustro.fin.ec>
2024-04-04 14:35:48 -0500

    Agregar animacion de text para presentacion de pagina

---
+++ components/Helper/TypeWriteEffect.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
+const TypeWriteEffect = () => {
+	return (
+		<TypeAnimation
+			sequence={[
+				// Same substring at the start will only be typed out once, initially
+				"Desarrollador Web",
+				1000, // wait 1s before replacing "Mice" with "Hamsters"
+				"Desarrollador Movil",
+				1000,
+				"Ciencias de datos",
+				1000,			
+			]}
+			wrapper='span'
+			speed={50}
+            className="xl:text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] text-white font-bold"
+			style={{ fontSize: "2em", display: "inline-block" }}
+			repeat={Infinity}
+		/>
+	);
+};
+
+export default TypeWriteEffect;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,40 @@
 import React from "react";
+import TypeWriteEffect from "./Helper/TypeWriteEffect";
 
 const Hero = () => {
 	return (
 		<div className='w-[100vw] pt-[4vh] md:pt-[12vh] h-[85vh] md:h-[100vh] custom-bg '>
-			Hero
+			<div className='flex justify-center flex-col w-[80%] h-[100%] mx-auto'>
+				<div className='grid grid-cols-1 md:grid-cols-2 gap-[3rem] items-center'>
+					<div className=''>
+						<h1 className='text-[#c4cfde] mb-[1.3rem]'>
+							BIENVENIDO A MI MUNDO
+						</h1>
+						<div>
+							<h1 className='x1:text-[50px] lg:text-[40px] sm:text-[35px] text-[25px] font-semibold leading-[2.6rem] text-white'>
+								Hola, soy <span className='text-blue-300'>Patrick</span>
+							</h1>
+							<TypeWriteEffect />
+						</div>
+						<p className='mt-[1.2rem] text-[15px] md:text-[17px] text-[#c4cfde]'>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+							similique in sint nemo, error quos saepe soluta est fugit delectus
+							ipsum. Deserunt quidem earum dolore voluptatibus laudantium fugit
+							sunt repudiandae.
+						</p>
+						<div className='mt-[2rem] flex items-center space-x-6'>
+							<button
+								className='relative  flex h-[50px] w-40 items-center justify-center font-semibold overflow-hidden bg-blue-500 text-white 
+							shadow-2x1 transition-all before:absolute before:h-0 before:w before:rounded-full before:bg-red-600 before:duration-500 
+							before:ease-out hover:before:h-56'
+							>
+								<span className='relative z-10'>Clic aqui</span>
+							</button>
+							<button className="before:ease relative h-12 w-40 overflow-hidden bg-orange-600 text-white font-semibold shadow-2x1 ">CV</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
