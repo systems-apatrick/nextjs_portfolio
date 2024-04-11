f2b4f482eab5be6b14569214b38fa2ae1e00868f
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2024-04-10 19:57:17 -0500

    agrega servicios

--- components/Helper/ServiceCard.tsx
+++ components/Helper/ServiceCard.tsx
@@ -1,34 +1,46 @@
 import {
-	BugAntIcon,
-	CodeBracketIcon,
-	CommandLineIcon,
-	CursorArrowRaysIcon,
-	RocketLaunchIcon,
-	ServerIcon,
+  BugAntIcon,
+  CodeBracketIcon,
+  CommandLineIcon,
+  CursorArrowRaysIcon,
+  RocketLaunchIcon,
+  ServerIcon,
 } from "@heroicons/react/16/solid";
 import React from "react";
 
 interface Props {
-	title: string;
-	num: string;
+  title: string;
+  num: string;
 }
 
 const iconMapping = {
-	"01": CommandLineIcon,
-	"02": ServerIcon,
-	"03": CodeBracketIcon,
-	"04": CursorArrowRaysIcon,
-	"05": RocketLaunchIcon,
-	"06": BugAntIcon,
+  "01": CommandLineIcon,
+  "02": ServerIcon,
+  "03": CodeBracketIcon,
+  "04": CursorArrowRaysIcon,
+  "05": RocketLaunchIcon,
+  "06": BugAntIcon,
 };
 const ServiceCard = ({ num, title }: Props) => {
-	const IconComponent =
-		(iconMapping as Record<string, React.ElementType>)[num] || CommandLineIcon;
-	return (
-		<div className='bg-black custom__service z-[100] relative transform rounded-2x1 text center p-6 shadow-md '>
-			ServiceCard
-		</div>
-	);
+  const IconComponent =
+    (iconMapping as Record<string, React.ElementType>)[num] || CommandLineIcon;
+  return (
+    <div className="bg-black custom__service z-[100] relative transform rounded-2x1 text-center p-6 shadow-md">
+      <IconComponent className="w-[5rem] relative z-[1] mx-auto h-[5rem] text-blue-400"></IconComponent>
+      <h1 className="text-[25px] relative z-[1] text-white mt-[1rem]">
+        {title}
+      </h1>
+      <p className="text-white text-[15px] opacity-60 mt-[0.8rem]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nulla,
+        ratione laborum iste sint facilis, temporibus nisi dignissimos enim
+        dolorum sunt eveniet quisquam assumenda, ipsam debitis totam expedita
+        nobis vel.
+      </p>
+      <p className="text-white text-[1.6rem] font-bold absolute top-3 right-4">
+        {num}
+      </p>
+    </div>
+  );
 };
 
 export default ServiceCard;
--- components/Services.tsx
+++ components/Services.tsx
@@ -2,21 +2,36 @@ import React from "react";
 import ServiceCard from "./Helper/ServiceCard";
 
 const Services = () => {
-	return (
-		<div className='pt-[5rem] pb-[3rem] bg-[#0b0c13]'>
-			<div className='text-center'>
-				<p className='heading__mini'>Popular Services</p>
-				<h1 className='heading__primary'>
-					My special <span className='text-blue-300'>services</span> for you
-				</h1>
-			</div>
-			<div className='pt-[5rem] w-[5rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center'>
-				<div>
-					<ServiceCard title='React Web site' num='01'/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="pt-[5rem] pb-[3rem] bg-[#0b0c13]">
+      <div className="text-center">
+        <p className="heading__mini">Popular Services</p>
+        <h1 className="heading__primary">
+          My special <span className="text-blue-300">services</span> for you
+        </h1>
+      </div>
+      <div className="pt-[5rem] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[3rem] items-center">
+        <div>
+          <ServiceCard title="React Web site" num="01" />
+        </div>
+        <div>
+          <ServiceCard title="React Web site" num="02" />
+        </div>
+        <div>
+          <ServiceCard title="React Web site" num="03" />
+        </div>
+        <div>
+          <ServiceCard title="React Web site" num="04" />
+        </div>
+        <div>
+          <ServiceCard title="React Web site" num="05" />
+        </div>
+        <div>
+          <ServiceCard title="React Web site" num="06" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
